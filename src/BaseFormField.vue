b481ececc1d986c4ea389f5306212e87ccda53bb
Amr Noman <amrnn90@gmail.com>
2020-03-31 06:25:30 +0300

    add simple BaseFormField component

---
+++ src/BaseFormField.vue
@@ -0,0 +1,43 @@
+<template>
+  <div ref="el" class="base-form-field">
+    <label :for="field.name" class="base-form-field__label">
+      {{ field.label }}
+    </label>
+
+    <slot
+      :input-props="field.inputProps"
+      :input-listeners="field.inputListeners"
+      class="base-form-field__input"
+    />
+
+    <div v-if="field.error" class="base-form-field__error">
+      {{ field.error }}
+    </div>
+  </div>
+</template>
+
+<script>
+import useFormField from "./useFormField";
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+  },
+  setup(props) {
+    const field = useFormField(props.name, {
+      label: props.label,
+    });
+
+    return { field };
+  },
+};
+</script>
+
+<style></style>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-export { default as useFormField } from "./useFormField";
 export { default as BaseForm } from "./BaseForm";
+export { default as BaseFormField } from "./BaseFormField";
+export { default as useFormField } from "./useFormField";
