632747bf40e17b164c56b0b24babb67898fe4c2d
Amr Noman <amrnn90@gmail.com>
2020-04-02 19:10:46 +0300

    1.0.4

--- src/utils/lodash.js
+++ src/utils/lodash.js
@@ -1,10 +1,10 @@
-import { cloneDeep } from "lodash/cloneDeep";
-import { isArray } from "lodash/isArray";
-import { isEqual } from "lodash/isEqual";
-import { isObject } from "lodash/isObject";
-import { set } from "lodash/set";
-import { get } from "lodash/get";
-import { omit } from "lodash/omit";
-import { pick } from "lodash/pick";
+import cloneDeep from "lodash/cloneDeep";
+import isArray from "lodash/isArray";
+import isEqual from "lodash/isEqual";
+import isObject from "lodash/isObject";
+import set from "lodash/set";
+import get from "lodash/get";
+import omit from "lodash/omit";
+import pick from "lodash/pick";
 
 export default { cloneDeep, isEqual, isObject, isArray, get, set, omit, pick };
